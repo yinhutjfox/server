09bad0958452df9323fcb9049f62a23468e978f5
Sergey Vojtovich <svoj@mariadb.org>
2018-12-09 14:00:55 +0400

    Call ha_table_exists() under SU lock

--- sql/sql_base.cc
+++ sql/sql_base.cc
@@ -3882,6 +3882,39 @@ end:
 }
 
 
+static bool upgrade_lock_if_not_exists(THD *thd,
+                                       const DDL_options_st &create_info,
+                                       TABLE_LIST *create_table,
+                                       ulong lock_wait_timeout)
+{
+  DBUG_ENTER("upgrade_lock_if_not_exists");
+
+  if (thd->lex->sql_command == SQLCOM_CREATE_TABLE ||
+      thd->lex->sql_command == SQLCOM_CREATE_SEQUENCE)
+  {
+    if (!create_info.or_replace() &&
+        ha_table_exists(thd, &create_table->db, &create_table->table_name))
+    {
+      if (create_info.if_not_exists())
+      {
+        push_warning_printf(thd, Sql_condition::WARN_LEVEL_NOTE,
+                            ER_TABLE_EXISTS_ERROR,
+                            ER_THD(thd, ER_TABLE_EXISTS_ERROR),
+                            create_table->table_name.str);
+      }
+      else
+        my_error(ER_TABLE_EXISTS_ERROR, MYF(0), create_table->table_name.str);
+      DBUG_RETURN(true);
+    }
+    DBUG_RETURN(thd->mdl_context.upgrade_shared_lock(
+                                   create_table->mdl_request.ticket,
+                                   MDL_EXCLUSIVE,
+                                   lock_wait_timeout));
+  }
+  DBUG_RETURN(false);
+}
+
+
 /**
   Acquire upgradable (SNW, SNRW) metadata locks on tables used by
   LOCK TABLES or by a DDL statement. Under LOCK TABLES, we can't take
@@ -3963,8 +3996,12 @@ lock_table_names(THD *thd, const DDL_options_st &options,
     DBUG_RETURN(FALSE);
 
   if (flags & MYSQL_OPEN_SKIP_SCOPED_MDL_LOCK)
+  {
     DBUG_RETURN(thd->mdl_context.acquire_locks(&mdl_requests,
-                                               lock_wait_timeout));
+                                               lock_wait_timeout) ||
+                upgrade_lock_if_not_exists(thd, options, tables_start,
+                                           lock_wait_timeout));
+  }
 
   /* Protect this statement against concurrent BACKUP STAGE or FTWRL. */
   if (thd->has_read_only_protection())
@@ -3974,6 +4011,8 @@ lock_table_names(THD *thd, const DDL_options_st &options,
   mdl_savepoint= thd->mdl_context.mdl_savepoint();
 
   while (!thd->mdl_context.acquire_locks(&mdl_requests, lock_wait_timeout) &&
+         !upgrade_lock_if_not_exists(thd, options, tables_start,
+                                     lock_wait_timeout) &&
          !thd->mdl_context.try_acquire_lock(&global_request))
   {
     if (global_request.ticket)
